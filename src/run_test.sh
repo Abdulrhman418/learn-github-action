4498b01510e74953e74fa2656f07f7fb43578e6a
abdulrhman <abdulrhmanmokhtar399@gmail.com>
2026-01-06 19:25:00 +0200

    SOLVE bash3

--- src/run_test.sh
+++ src/run_test.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-
 OUTPUT=$(node src/app.js)
+
 
 EXPECTED="Hello, Abdulrahman!"
 
 
-if [ "$OUTPUT" == "$EXPECTED" ]; then
+if [ "$OUTPUT" = "$EXPECTED" ]; then
     echo "✅ Test Passed!"
 else
     echo "❌ Test Failed!"
     echo "Expected: $EXPECTED"
     echo "Got: $OUTPUT"
-fi
+    exit 1 
