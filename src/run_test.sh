f51e71429b01e51b77db639c042ef6712b84f05c
abdulrhman <abdulrhmanmokhtar399@gmail.com>
2026-01-06 19:50:21 +0200

    SOLVE bash5

--- src/run_test.sh
+++ src/run_test.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# تشغيل app.js وتخزين الناتج
+
 OUTPUT=$(node src/app.js)
 
-# القيمة المتوقعة
+
 EXPECTED="Hello, Abdulrahman!"
 
-# اختبار الناتج
 if [ "$OUTPUT" = "$EXPECTED" ]; then
     echo "✅ Test Passed!"
 else
     echo "❌ Test Failed!"
     echo "Expected: $EXPECTED"
     echo "Got: $OUTPUT"
-    exit 1  # يرجع خطأ لكي يفشل GitHub Action في حالة الفشل
+    exit 1  
 fi
