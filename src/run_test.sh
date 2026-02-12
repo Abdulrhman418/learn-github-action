fbfab24154fde52aab0b59ba3e05b092d46cee63
abdulrhman <abdulrhmanmokhtar399@gmail.com>
2026-02-12 22:02:50 +0200

    add use-cash.yaml

---
+++ src/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+OUTPUT=$(node src/app.js)
+
+
+EXPECTED="Hello, Abdulrahman!"
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+    echo "✅ Test Passed!"
+else
+    echo "❌ Test Failed!"
+    echo "Expected: $EXPECTED"
+    echo "Got: $OUTPUT"
+    exit 1  
+fi
